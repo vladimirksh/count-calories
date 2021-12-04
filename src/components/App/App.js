78a82735a139276ede4ba0b607b2549f024c30d4
Vladimir Kashankov <74548402+vladimirksh@users.noreply.github.com>
2021-12-04 19:11:50 +0300

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Header from '../Header/Header';
 import Diary from '../Diary/Diary';
 import Tips from '../Tips/Tips';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 import NavBar from '../NavBar/NavBar';
 
 
